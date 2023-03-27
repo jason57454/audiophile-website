8f47c968fd5e80aa2c5e856a957d9158c5dfdc91
jason <jason.chambrin@mailo.com>
2023-03-27 22:47:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var menu = document.querySelector(".hamburger");
+var toggleMenu = document.querySelector(".menu_layer");
+var hero = document.querySelector(".main");
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("hamburger_active");
+  toggleMenu.classList.toggle("active");
+  if (toggleMenu.classList.contains("active")) {
+    hero.style.filter = "brightness(40%)";
+  } else {
+    hero.style.filter = "brightness(100%)";
+  }
 });
