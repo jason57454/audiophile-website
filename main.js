680ee40205d16e657f52aa4b182f2478168802a9
jason <jason.chambrin@mailo.com>
2023-03-29 20:48:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
-var menu = document.querySelector(".hamburger");
-var toggleMenu = document.querySelector(".menu_layer");
-var hero = document.querySelector(".main");
-var cartIcon = document.querySelector(".cart_icon");
-var cart = document.querySelector(".cart");
+function displayMenu() {
+  var menu = document.querySelector(".hamburger");
+  var toggleMenu = document.querySelector(".menu_layer");
+  var hero = document.querySelector(".main");
+  var cartIcon = document.querySelector(".cart_icon");
+  var cart = document.querySelector(".cart");
 
-menu.addEventListener("click", () => {
-  menu.classList.toggle("hamburger_active");
-  toggleMenu.classList.toggle("active");
-  if (toggleMenu.classList.contains("active")) {
-    cartIcon.classList.remove("active");
-    cart.style.display = "none";
-    hero.style.filter = "brightness(40%)";
-  } else {
-    hero.style.filter = "brightness(100%)";
-  }
-});
+  menu.addEventListener("click", () => {
+    menu.classList.toggle("hamburger_active");
+    toggleMenu.classList.toggle("active");
+    if (toggleMenu.classList.contains("active")) {
+      cartIcon.classList.remove("active");
+      cart.style.display = "none";
+      hero.style.filter = "brightness(40%)";
+    } else {
+      hero.style.filter = "brightness(100%)";
+    }
+  });
+}
 
-cartIcon.addEventListener("click", () => {
-  cartIcon.classList.toggle("active");
-  if (cartIcon.classList.contains("active")) {
-    cart.style.display = "block";
-    menu.classList.remove("hamburger_active");
-    toggleMenu.classList.remove("active");
-    hero.style.filter = "brightness(40%)";
-  } else {
-    cart.style.display = "none";
-    hero.style.filter = "brightness(100%)";
-  }
-});
+function displayCart() {
+  var menu = document.querySelector(".hamburger");
+  var toggleMenu = document.querySelector(".menu_layer");
+  var hero = document.querySelector(".main");
+  var cartIcon = document.querySelector(".cart_icon");
+  var cart = document.querySelector(".cart");
+
+  cartIcon.addEventListener("click", () => {
+    cartIcon.classList.toggle("active");
+    if (cartIcon.classList.contains("active")) {
+      cart.style.display = "block";
+      menu.classList.remove("hamburger_active");
+      toggleMenu.classList.remove("active");
+      hero.style.filter = "brightness(40%)";
+    } else {
+      cart.style.display = "none";
+      hero.style.filter = "brightness(100%)";
+    }
+  });
+}
+
+displayMenu();
+displayCart();
